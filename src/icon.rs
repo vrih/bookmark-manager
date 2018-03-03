d688700f3462415294517ffd7afe2977c3c18bb7
Daniel Bowman <daniel@danielbowman.co.uk>
2018-03-03 12:10:50 +0000

    Mobile icon (#10)
    * Fetch mobile icons

--- src/icon.rs
+++ src/icon.rs
@@ -14,12 +14,17 @@ use reqwest;
 
 use url::Url;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Icon{
     x: u16,
     y: u16,
-    href: String
+    href: String,
+    poor: bool
 }
+
+const DESKTOP_UA: &str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36";
+
+const MOBILE_UA: &str = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Mobile Safari/537.36";
 
 fn attr_parser(doc: &Document, attr: &str, val: &str) -> Vec<Icon>{
     let mut links: Vec<Icon> = Vec::new();
@@ -42,12 +47,12 @@ fn attr_parser(doc: &Document, attr: &str, val: &str) -> Vec<Icon>{
         let x = x_y[0].parse::<u16>().unwrap();
         let y = x_y[0].parse::<u16>().unwrap();
 
-        links.push(Icon{x, y, href: String::from(link.attr("href").unwrap())});
+        links.push(Icon{x, y, href: String::from(link.attr("href").unwrap()), poor: false});
     };
     links
 }
 
-fn get_image_paths(doc: &Document) -> Option<String>{
+fn get_image_paths(doc: &Document) -> Option<Vec<Icon>>{
     let mut links: Vec<Icon> = Vec::new();
     
     links.extend(attr_parser(doc, "rel", "icon"));
@@ -58,17 +63,15 @@ fn get_image_paths(doc: &Document) -> Option<String>{
 
     if links.len() == 0{
         for link in doc.find(Name("meta").and(Attr("property", "og:image"))).collect::<Vec<Node>>(){
-            links.push(Icon{x: 1, y: 1, href: String::from(link.attr("content").unwrap())});
+            links.push(Icon{x: 1, y: 1, href: String::from(link.attr("content").unwrap()), poor: true});
         };
     };
 
-    
-    let out = links.last();
-    let out = match out{
-        Some(links) => Some(links.href.clone()),
-        None => None
+    if links.len() == 0{
+        return None
+    } else {
+        return Some(links)
     };
-    out 
 }
 
 fn url_from_paths(root: &str, path: &str) -> String{
@@ -80,32 +83,73 @@ fn url_from_paths(root: &str, path: &str) -> String{
     
     if image_url.len() >= 4 && &image_url[..4] == "http"{
        return image_url
-    }              
+   }              
     
     let root_url = Url::parse(&root).unwrap();
     let parsed = root_url.join(&path).unwrap();
     String::from(parsed.as_str())
 }
 
-pub fn download_image(url: &str, fs_path: &str) -> Result<(), reqwest::Error>{
+fn document_for_ua(url: &str, ua: &str) -> Result<(String, Document), reqwest::Error>{
     let mut headers = header::Headers::new();
-    headers.set(header::UserAgent::new("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36".to_string()));
+    headers.set(header::UserAgent::new(ua.to_string()));
 
     // get a client builder
     let client = Client::builder()
         .default_headers(headers)
         .build().expect("Bob");
     let mut resp = try!(client.get(url)
-        .header(header::UserAgent::new("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36".to_string()))
+        .header(header::UserAgent::new(ua.to_string()))
         .send());
-    
+
+    let urlout = String::from(resp.url().as_str());
     let body = try!(resp.text());
 
-    let document = Document::from(&body[..]);
+    return Ok((urlout, Document::from(&body[..])));
+}
 
-    match get_image_paths(&document) {
+
+
+pub fn download_image(url: &str, fs_path: &str) -> Result<(), reqwest::Error>{
+
+    let (mut final_url, document) = try!(document_for_ua(url, DESKTOP_UA));
+
+    let icon: Option<Icon> = match get_image_paths(&document) {
+        Some(icons) => {
+            let out = icons.last().unwrap().clone();
+            if out.x > 128{
+                Some(out.clone())
+            } else {
+                let (a, md) = try!(document_for_ua(url, MOBILE_UA));
+                final_url = a;
+                match get_image_paths(&md) {
+                    Some(mut mis) => {
+                        mis.sort_by_key(|a| a.x);
+                        Some(mis.last().unwrap().clone())
+
+                    },
+                    None => Some(icons.last().unwrap().clone())
+                }
+            }
+        },
+        None => {
+            let (a, md) = try!(document_for_ua(url, MOBILE_UA));
+            final_url = a;
+            match get_image_paths(&md) {
+                Some(mis) => Some(mis.last().unwrap().clone()),
+                None => None
+            }
+        }
+    };
+
+    let icon_url = match icon{
+        Some(links) => Some(links.href.clone()),
+        None => None
+    };
+
+    match icon_url {
         Some(image_url) => { 
-            let download_url = url_from_paths(url, &image_url);
+            let download_url = url_from_paths(&final_url, &image_url);
             download_media(&download_url, fs_path);}
         _  => println!("No image for {}", url)          
     };
